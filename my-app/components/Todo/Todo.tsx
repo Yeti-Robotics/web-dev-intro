f370d82dfa1b3383d9287804db715a757674763c
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2021-12-01 07:27:06 -0500

    Added comments to Todo.tsx

--- my-app/components/Todo/Todo.tsx
+++ my-app/components/Todo/Todo.tsx
@@ -3,16 +3,16 @@ import React from 'react';
 interface TodoProps {
 	text: string;
 	state: [string[], React.Dispatch<React.SetStateAction<string[]>>];
-	num: number;
+	num: number; // The index of this Todo in the todos array
 }
 
 const Todo: React.VFC<TodoProps> = ({ text, state, num }) => {
 	const [todos, setTodos] = state;
 
 	const onClose = () => {
-		const newTodos = [...todos];
-		newTodos.splice(num, 1);
-		setTodos(newTodos);
+		const newTodos = [...todos]; // Create a copy of our todos
+		newTodos.splice(num, 1); // Use this Todo's index to delete it in the new array
+		setTodos(newTodos); // set new array as the state shown on the website
 	};
 
 	return (
